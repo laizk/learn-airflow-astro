2a13a597c3403cde17d35db7257dc4c7fe4b5096
t0pZ <kristoffer.laiz@outlook.com>
2025-09-19 10:45:42 +0800

    Old way to create dag (DO NOT DO)

--- dags/my_dag.py
+++ dags/my_dag.py
@@ -1,15 +1,15 @@
 from airflow.sdk import dag, task, DAG
 from pendulum import datetime
 
-with DAG(
+d = DAG(
     schedule="@daily",
     start_date=datetime(2025, 1, 1),
     description="This dags does...",
     tags=["team_a", "source_a"],
     max_consecutive_failed_dag_runs=3
-):
+    )
 
-    @task
+    @task(dag=d)
     def task_a():
         print("Hello from task A!")
         
