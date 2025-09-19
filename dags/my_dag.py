cfc8b87ecab53bfc73aa94f9d313c1ce4b532f3e
t0pZ <kristoffer.laiz@outlook.com>
2025-09-19 10:56:17 +0800

    Add default_args wih retries prop

--- dags/my_dag.py
+++ dags/my_dag.py
@@ -1,14 +1,17 @@
 from airflow.sdk import dag, task
 from pendulum import datetime
 
-
+default_args ={
+    'retries': 3
+}
 
 @dag(
     schedule="@daily",
     start_date=datetime(2025, 1, 1),
     description="This dags does...",
     tags=["team_a", "source_a"],
-    max_consecutive_failed_dag_runs=3
+    max_consecutive_failed_dag_runs=3,
+    default_args=default_args,
 )
 def my_dag():
     
